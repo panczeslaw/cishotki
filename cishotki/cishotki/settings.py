42c4b8c943a850902bfdaed23e006a0e5b9c3e39
panczeslaw <vkozlovsk@gmail.com>
2019-11-11 19:59:26 +0300

    Last commit

--- cishotki/cishotki/settings.py
+++ cishotki/cishotki/settings.py
@@ -91,10 +91,14 @@ AUTHENTICATION_BACKENDS = [
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cishotki',
+        'USER': 'vkozlovsk',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
